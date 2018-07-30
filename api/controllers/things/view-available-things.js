bf0ad94595c0a9cf6f905c20325228301daaf712
MarcServat <marcservat@hotmail.com>
2018-07-30 15:23:41 +0200

    WIP

---
+++ api/controllers/things/view-available-things.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+
+  friendlyName: 'View available things',
+
+
+  description: 'Display "Available things" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/things/available-things'
+    }
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    var things = await Thing.find();
+    // Respond with view.
+    return exits.success({
+      things
+    });
+
+  }
+
+};
